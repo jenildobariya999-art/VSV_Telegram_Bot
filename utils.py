8c5cf7f11def80d4910f93f707c49335f3745c28
jenildobariya999-art <jenildobariya999@gmail.com>
2026-02-27 21:12:54 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,10 @@
+import requests
+from config import VSV_TOKEN
 
+def vsv_transfer(paytm_number, amount, comment):
+    url = f"https://vsv-gateway-solutions.co.in/Api/api.php?token={VSV_TOKEN}&paytm={paytm_number}&amount={amount}&comment={comment}"
+    try:
+        response = requests.get(url)
+        return response.json()
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
