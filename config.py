9cc8c061fc14bbe639858471d79a8c2209a1c77d
jenildobariya999-art <jenildobariya999@gmail.com>
2026-02-27 21:12:04 +0530

    config.py

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
+import os
 
+API_TOKEN = os.environ.get("8534393299:AAFLYuQiqImk6wWI6TLTYrR_7xKbgwZvK_8")  # Telegram Bot Token
+VSV_TOKEN = os.environ.get("TVJZCUHK")  # TVJZCUHK token
+ADMIN_IDS = [6925391837]  # Your Telegram ID
